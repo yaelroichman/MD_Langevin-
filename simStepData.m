7c14c380b0e792333f1cfac5ba64b97f60b83d03
Zaretsky-Daniel <45542984+Zaretsky-Daniel@users.noreply.github.com>
2020-04-13 12:28:47 +0300

    Update simStepData.m
    added trap parameters

--- simStepData.m
+++ simStepData.m
@@ -1,8 +1,12 @@
 classdef simStepData < handle
     properties
         stepNum % The numer of the current step in the sim
+        
         particlePositions % The positions of the particles in the current step
         trapPositions % The positions of the traps in the current step
+        A % The depth of the trap
+        s % The size of the trap
+        
         wallPositionsX % The x positions of the walls, if applicable
         wallPositionsY % The y positions of the walls, if applicable
     end
